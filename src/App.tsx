5e625cc5c6234bf44c5a35ee2a6c9b58a782c229
Ali Rahmani <alirahmani.prog@gmail.com>
2021-11-18 00:30:55 +0330

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { GlobalProvider } from "./providers/global"
 
 import Collections from "./components/collections/Collections"
 import Relations from "./components/relations/Relations"
+import Icon from "./components/icon/Icon"
 
 function App() {
   return (
@@ -9,6 +10,19 @@ function App() {
       <div className='app'>
         <Collections />
         <Relations />
+
+        <footer>
+          With
+          <span className='love'>
+            <Icon name='heart' />
+          </span>
+          By
+          <strong>
+            <a href='https://rahmaniali.ir' target='_blank' rel='noreferrer'>
+              Ali Rahmani
+            </a>
+          </strong>
+        </footer>
       </div>
     </GlobalProvider>
   )
